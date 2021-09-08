fc325e272e81a33bce866dd0a6ad055e50b761a6
Admin <Admin@ADMIN>
2021-09-08 16:53:38 +0700

    Add project files.

---
+++ DEMOMVC/Models/LapTrinhQuanLyDBcontext.cs
@@ -0,0 +1,25 @@
+using System;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Data.Entity;
+using System.Linq;
+
+namespace DEMOMVC.Models
+{
+    public partial class LapTrinhQuanLyDBcontext : DbContext
+    {
+        public LapTrinhQuanLyDBcontext()
+            : base("name=LapTrinhQuanLyDBcontext")
+        {
+        }
+
+        public virtual DbSet<Account> Accounts { get; set; }
+        public virtual DbSet<Student> Students { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Account>()
+                .Property(e => e.Password)
+                .IsUnicode(false);
+        }
+    }
+}
