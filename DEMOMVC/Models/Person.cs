921068f71eb50ecab91decaa0289f47ca87e8e93
Huy-Loc <huyloc120199@gmail.com>
2021-09-10 14:51:30 +0700

    new

--- DEMOMVC/Models/Person.cs
+++ DEMOMVC/Models/Person.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -8,6 +9,9 @@ namespace DEMOMVC.Models
     public class Person
     {
         public string PersonID { get; set; }
+        [Required(ErrorMessage = "ID không được để trống")]
         public string PersonName { get; set; }
+        [Required(ErrorMessage = "Tên không được để trống")]
+        [MinLength(3)]
     }
 }
